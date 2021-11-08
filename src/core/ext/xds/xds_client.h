78485532556703e422fa78d2be8d86366930bb17
markdroth <markdroth@users.noreply.github.com>
2021-11-08 18:30:46 +0000

    Automated change: Fix sanity tests

--- src/core/ext/xds/xds_client.h
+++ src/core/ext/xds/xds_client.h
@@ -230,7 +230,7 @@ class XdsClient : public DualRefCounted<XdsClient> {
                          const XdsApi::ResourceName& resource)
         ABSL_EXCLUSIVE_LOCKS_REQUIRED(&XdsClient::mu_);
     void UnsubscribeLocked(const std::string& type_url,
-                           const XdsApi::ResourceName& resource,
+                           const XdsApi::ResourceName& name,
                            bool delay_unsubscription)
         ABSL_EXCLUSIVE_LOCKS_REQUIRED(&XdsClient::mu_);
 
