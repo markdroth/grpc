fcbc9e9716610b2bb988cadd0566123569f72159
markdroth <markdroth@users.noreply.github.com>
2023-09-12 01:28:34 +0000

    Automated change: Fix sanity tests

--- test/core/client_channel/lb_policy/round_robin_test.cc
+++ test/core/client_channel/lb_policy/round_robin_test.cc
@@ -14,8 +14,6 @@
 // limitations under the License.
 //
 
-#include <stddef.h>
-
 #include <array>
 
 #include "absl/status/status.h"
@@ -26,7 +24,6 @@
 #include <grpc/grpc.h>
 
 #include "src/core/lib/gprpp/orphanable.h"
-#include "src/core/lib/gprpp/ref_counted_ptr.h"
 #include "src/core/lib/load_balancing/lb_policy.h"
 #include "test/core/client_channel/lb_policy/lb_policy_test_lib.h"
 #include "test/core/util/test_config.h"
